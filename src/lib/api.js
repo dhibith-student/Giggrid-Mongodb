ca4d121fee12931c5313e5acb1265c6d34348e80
dhibith-student <dhibith.s2024@vitstudent.ac.in>
2026-04-16 23:00:45 +0530

    render url env change version

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 const TOKEN_KEY = "giggrid_token";
 
 function getHeaders(includeJson = true) {
